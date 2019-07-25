200cf4b0c1890547f85d03fa8527eb7b47d38b76
Fran <sophie.fran22@gmail.com>
2019-07-25 16:44:40 -0300

    Modelar banco de dados

--- ideias/models.py
+++ ideias/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    GENEROS= (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('o', 'Outro'),
+    )
+    nome = models.CharField(
+        max_length=255,
+        verbose_name='Nome'
+    )
+
+    sobrenome = models.CharField(
+        max_length=255,
+        verbose_name='Sobrenome'
+    )
+
+    genero = models.CharField(
+        max_length=255,
+        verbose_name='Generos',
+        choices=GENEROS
+
+    )
+
+    email = models.EmailField(
+        max_length=255,
+        verbose_name='E-mail'
+    )
+
+    biografia = models.TextField(
+        null=True
+        blank=null
+    )
+
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome + ' ' + self.sobrenome
--- ideias/views.py
+++ ideias/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from ideias import Pessoa
 
 # Create your views here.
+
 def index(request):
     context = {
-
+    if request.method == 'post':
+        pessoa = Pessoa()
+        pessoa.nome = request.post.get('nome')
+        pessoa.sobrenome = request.post.get('sobrenome')
+        pessoa.email = request.post.get('email')
+        pessoa.genero = request.post.get('genero')
+        pessoa.biografia = request.post.get('biografia')
+        pessoa.save()
+        context = {'msg', 'Parabéns :)'}
     }
     return render(request, 'index.html', context)
 
 def sobre(request):
+    pessoa = Pessoa.objects
     context = {
 
     }
